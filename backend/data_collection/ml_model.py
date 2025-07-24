8f36849f19cf65939ab4cd907f164256742e828d
unknown <akim9165@gmail.com>
2025-07-23 19:08:19 -0700

    60 nodes give best accuracy

--- backend/data_collection/ml_model.py
+++ backend/data_collection/ml_model.py
@@ -1,29 +1,44 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-import os
-
-# Get the directory where the current script is located
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Build the full path to the CSV file
-file_path = os.path.join(script_dir, "ArenaData.csv")
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 
-data = pd.read_csv('ArenaData.csv')
+data = pd.read_csv('backend/data_collection/ArenaData.csv')
 
-y = data.isWinner # select target
+# New column named Wins, access placement column, and apply a lambda x, for each x the function checks if x is in [1,2,3,4], return 1, or else return 0
+data['Wins'] = data['placement'].apply(lambda x: 1 if x in [1,2,3,4] else 0)
+
+y = data['Wins'] # select target
 
 # choosing features I want to train my model on 
 features = ['kills', 'deaths', 'assists', 'totalDamageDealt', 'totalDamageTaken', 'goldEarned']
 X = data[features]
 
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.2, random_state = 42)
 
-forest_model = RandomForestRegressor(random_state = 1)
-forest_model.fit(train_X, train_y)
-prediction = forest_model.predict(val_X)
-print(mean_absolute_error(val_y, prediction))
+def get_accuracy(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    forest_model = RandomForestClassifier(class_weight='balanced', random_state=1, max_leaf_nodes=max_leaf_nodes)
+    forest_model.fit(train_X, train_y)
+    prediction = forest_model.predict(val_X)
+    accuracy =  accuracy_score(val_y, prediction)
+    report = classification_report(val_y, prediction)
+    importances = forest_model.feature_importances_
+    return accuracy, report, importances
+
+for max_leaf_nodes in [10, 20, 30, 40, 50, 60]:
+    my_accuracy, report, importances = get_accuracy(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf node: %d \t Accuracy: %.2f" %(max_leaf_nodes, my_accuracy))
+    print("Classification Report: \n", report)
+
+    print("Feature Importances:")
+    for feature, importance in zip(features, importances):
+        print(f"{feature}: {importance:.4f}")
+    print('-' * 50)
+
+    
+
+    # 60 nodes gives best accuracy with 0.89
+
 
 
